0ca071968d3f976a77fae165bcb60584be52c614
hafizovb <hbulat35@gmail.com>
2023-04-01 19:44:21 +0300

    add usdbufvsdgu

--- Entities/SkillInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Portfolio.Entities
-{
-    public class SkillInfo
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public int Percent { get; set; }
-    }
-}
